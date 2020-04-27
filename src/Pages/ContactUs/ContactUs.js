7948f727bc20abb61dbb16c63ae6c788d2fd68b7
linalys <lyssoudi@csd.auth.gr>
2020-04-27 19:12:16 +0300

    translated pricing and contact us

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -12,15 +12,40 @@ import {
     Button, Container,
 } from 'reactstrap';
 import {MDBIcon} from "mdbreact";
+import LocalizedStrings from "react-localization";
+import {useSelector} from "react-redux";
+
+let langStrings = new LocalizedStrings({
+    en:{
+        contact:"Contact Us",
+        name : "Name:",
+        email:"Email:",
+        sub:"Subject:",
+        message:"Message:",
+        send:"Send"
+    },
+
+    gr:{
+        contact:"Επικοινωνήστε μαζί μας",
+        name : "Όνομα:",
+        email:"Email:",
+        sub:"Θέμα:",
+        message:"Μήνυμα:",
+        send:"Αποστολή"
+    }
+
+})
+
 
 function ContactUs() {
+    langStrings.setLanguage(useSelector(state => state.language));
     return (
         <div>
             <Header/>
             <br/>
             <div>
                 <Container className="d-flex flex-column text-white text-center">
-                    <h1>Contact Us</h1>
+                    <h1>{langStrings.contact}</h1>
                 </Container>
                 <Container>
                     <Col md={9}>
@@ -28,34 +53,36 @@ function ContactUs() {
                             <CardBody>
                                 <FormGroup>
                                     <Label for="exampleText" size="lg">
-                                        Name:
+                                        <p className="text">{langStrings.name}</p>
                                     </Label>
                                     <input type="text" name="text" id="exampleText" className="bg-dark text-white"/>
                                 </FormGroup>
                                 <FormGroup>
                                     <Label for="exampleText" size="lg">
-                                        Email:
+                                        <p className="text">{langStrings.email}</p>
                                     </Label>
                                     <input type="text" name="text" id="exampleText" className="bg-dark text-white"/>
                                 </FormGroup>
                                 <FormGroup>
                                     <Label for="exampleText" size="lg">
-                                        Subject:
+                                        <p className="text">{langStrings.sub}</p>
                                     </Label>
                                     <input type="text" name="text" id="exampleText" className="bg-dark text-white"/>
                                 </FormGroup>
                                 <FormGroup>
-                                    <Label for="exampleText">
-                                        <MDBIcon far icon="envelope" className="ml-2"/>
-                                        Message :</Label>
+                                    <Label for="exampleText" size="lg">
+                                        <p className="text">{langStrings.message}
+                                            <MDBIcon far icon="envelope" className="ml-2"/></p>
+                                    </Label>
                                     <Input type="textarea" name="text" id="exampleText2" style={{height: 300}}
-                                    className="bg-dark text-white"/>
+                                           className="bg-dark text-white"/>
                                 </FormGroup>
                                 <FormGroup>
                                     <div>
-                                        <Button>
-                                            Send
+                                        <Button size="lg">
+                                            <p className="text" >{langStrings.send}
                                             <MDBIcon far icon="paper-plane" className="ml-2"/>
+                                            </p>
                                         </Button>
                                     </div>
                                     <br></br>
--- src/Pages/Pricing/Pricing.js
+++ src/Pages/Pricing/Pricing.js
@@ -3,44 +3,82 @@ import {PricingTable, PricingSlot, PricingDetail} from 'react-pricing-table';
 import Header from "../../Header/Header";
 import Footer from "../../Footer/Footer";
 import {Container} from "reactstrap";
+import LocalizedStrings from "react-localization";
+import {useSelector} from "react-redux";
 
 
-class Pricing extends React.Component {
+let langStrings = new LocalizedStrings({
+    en:{
+        pricing:"Pricing",
+        signup:"SIGN UP",
+        free:"FREE",
+        price1:"0€ per month",
+        price2:"15€ per month",
+        price3:"30€ per month",
+        langf:"Up to 1 Programming Language",
+        langb:"Up to 10 Programming Languages",
+        langp:"Unlimited Programming Languages",
+        testf:"One test per lesson",
+        testb:"Multiple test per lesson, escalating difficulty",
+        proff:"No access to advanced lessons",
+        profp:"Access to advanced lessons",
+        basic:"BASIC",
+        prof:"PROFESSIONAL"
 
-    render() {
-        return (
+    },
+
+    gr:{
+        pricing:"Τιμοκατάλογος",
+        signup:"ΕΓΓΡΑΦΗ",
+        free:"ΔΩΡΕΑΝ",
+        price1:"0€ ανά μήνα",
+        price2:"15€ ανά μήνα",
+        price3:"30€ ανά μήνα",
+        langf:"Μέχρι 1 Γλώσσα Προγραμματισμού",
+        langb:"Μέχρι 10 Γλώσσες Προγραμματισμού",
+        langp:"Απεριόριστες Γλώσσες Προγραμματισμού",
+        testf:"1 Τεστ ανά μάθημα",
+        testb:"Πολλαπλά Τεστ ανά μάθημα, με διαβαθμιζόμενη δυσκολία",
+        proff:"Χωρίς πρόσβαση σε προχωρημένα μαθήματα",
+        profp:"Με πρόσβαση σε προχωρημένα μαθήματα",
+        basic:"ΒΑΣΙΚΟ",
+        prof:"ΕΠΑΓΓΕΛΜΑΤΙΚΟ"
+    }
+
+})
+
+function Pricing() {
+    langStrings.setLanguage(useSelector(state => state.language));
+    return (
             <>
                 <Header/>
                 <br/>
-                <h1 className="text-white text-center">Pricing</h1>
+                <h1 className="text-white text-center">{langStrings.pricing}</h1>
                 <br/>
                 <PricingTable highlightColor='#ffff'>
                     <Container className="bg-dark text-white">
-                        <PricingSlot buttonText='TRY IT FREE' title='FREE' priceText='$0/month'>
-                            <PricingDetail> 15 projects</PricingDetail>
-                            <PricingDetail> <b>5 GB</b> storage</PricingDetail>
-                            <PricingDetail> <b>5</b> users</PricingDetail>
-                            <PricingDetail strikethrough> <b>Time tracking</b></PricingDetail>
+                        <PricingSlot buttonText={langStrings.signup} title={langStrings.free} priceText={langStrings.price1}>
+                            <PricingDetail> {langStrings.langf}</PricingDetail>
+                            <PricingDetail> {langStrings.testf}</PricingDetail>
+                            <PricingDetail strikethrough> {langStrings.profp}</PricingDetail>
                         </PricingSlot>
-                        <PricingSlot highlighted buttonText='SIGN UP' title='BASIC' priceText='$24/month'
+                        <PricingSlot highlighted buttonText={langStrings.signup} title={langStrings.basic} priceText={langStrings.price2}
                                      highlightColor='#f44336	'>
-                            <PricingDetail> <b>35</b> projects</PricingDetail>
-                            <PricingDetail> <b>15 GB</b> storage</PricingDetail>
-                            <PricingDetail> <b>Unlimited</b> users</PricingDetail>
-                            <PricingDetail> <b>Time tracking</b></PricingDetail>
+                            <PricingDetail> {langStrings.langb}</PricingDetail>
+                            <PricingDetail> {langStrings.testb}</PricingDetail>
+                            <PricingDetail strikethrough>{langStrings.profp}</PricingDetail>
                         </PricingSlot>
-                        <PricingSlot buttonText='SIGN UP' title='PROFESSIONAL' priceText='$99/month'>
-                            <PricingDetail> <b>100</b> projects</PricingDetail>
-                            <PricingDetail> <b>30 GB</b> storage</PricingDetail>
-                            <PricingDetail> <b>Unlimited</b> users</PricingDetail>
-                            <PricingDetail> <b>Time tracking</b></PricingDetail>
+                        <PricingSlot buttonText={langStrings.signup} title={langStrings.prof} priceText={langStrings.price3}>
+                            <PricingDetail> {langStrings.langp}</PricingDetail>
+                            <PricingDetail> {langStrings.testb}</PricingDetail>
+                            <PricingDetail> {langStrings.profp}</PricingDetail>
                         </PricingSlot>
                     </Container>
                 </PricingTable>
                 <Footer/>
             </>
         )
-    }
+
 }
 
 export default Pricing;
